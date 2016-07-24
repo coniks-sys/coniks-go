77dbf084143cbab11d97c7d3b01f57570e1775b8
Vu Quoc Huy <huyvq.c633@gmail.com>
2016-07-25 02:13:49 +0700

    Public STR and TB fields for verification
     * Resolves #24

--- merkletree/policy.go
+++ merkletree/policy.go
@@ -14,6 +14,8 @@ type Policies interface {
 }
 
 type ConiksPolicies struct {
+	LibVersion    string
+	HashID        string
 	vrfPrivateKey *[vrf.SecretKeySize]byte
 	EpochDeadline TimeStamp
 }
@@ -22,17 +24,19 @@ var _ Policies = (*ConiksPolicies)(nil)
 
 func NewPolicies(epDeadline TimeStamp, vrfPrivKey *[vrf.SecretKeySize]byte) Policies {
 	return &ConiksPolicies{
-		EpochDeadline: epDeadline,
+		LibVersion:    Version,
+		HashID:        crypto.HashID,
 		vrfPrivateKey: vrfPrivKey,
+		EpochDeadline: epDeadline,
 	}
 }
 
 // Serialize encodes the policy to a byte array with the following format:
-// [lib version, cryptographic algorithm in use, epoch deadline]
+// [lib version, cryptographic algorithm in use, epoch deadline, vrf public key]
 func (p *ConiksPolicies) Serialize() []byte {
 	var bs []byte
-	bs = append(bs, []byte(Version)...)                            // lib Version
-	bs = append(bs, []byte(crypto.HashID)...)                      // cryptographic algorithms in use
+	bs = append(bs, []byte(p.LibVersion)...)                       // lib Version
+	bs = append(bs, []byte(p.HashID)...)                           // cryptographic algorithms in use
 	bs = append(bs, util.ULongToBytes(uint64(p.EpochDeadline))...) // epoch deadline
 	bs = append(bs, vrf.Public(p.vrfPrivateKey)...)                // vrf public key
 	return bs
--- merkletree/tb.go
+++ merkletree/tb.go
@@ -1,7 +1,23 @@
 package merkletree
 
+import "github.com/coniks-sys/coniks-go/crypto"
+
 type TemporaryBinding struct {
 	Index     []byte
 	Value     []byte
 	Signature []byte
+}
+
+func NewTB(key crypto.SigningKey, index, indexProof, value, str []byte) *TemporaryBinding {
+	tb := make([]byte, 0, len(str)+len(index)+len(value))
+	tb = append(tb, str...)
+	tb = append(tb, index...)
+	tb = append(tb, value...)
+	sig := crypto.Sign(key, tb)
+
+	return &TemporaryBinding{
+		Index:     index,
+		Value:     value,
+		Signature: sig,
+	}
 }
