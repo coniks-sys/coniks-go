9fb844cddd8eebbf1ef1525d73789d351aa7b54b
Vu Quoc Huy <huyvq.c633@gmail.com>
2016-07-26 15:35:56 +0700

    Use Policies.Iterate to get its fields at once

--- merkletree/policy.go
+++ merkletree/policy.go
@@ -1,6 +1,8 @@
 package merkletree
 
 import (
+	"reflect"
+
 	"github.com/coniks-sys/coniks-go/crypto"
 	"github.com/coniks-sys/coniks-go/crypto/vrf"
 	"github.com/coniks-sys/coniks-go/utils"
@@ -9,6 +11,7 @@ import (
 type TimeStamp uint64
 
 type Policies interface {
+	Iterate() map[string][]byte
 	Serialize() []byte
 	vrfPrivate() *[vrf.SecretKeySize]byte
 }
@@ -29,6 +32,27 @@ func NewPolicies(epDeadline TimeStamp, vrfPrivKey *[vrf.SecretKeySize]byte) Poli
 		vrfPrivateKey: vrfPrivKey,
 		EpochDeadline: epDeadline,
 	}
+}
+
+// Iterate returns a map of exported fields' name to their values
+func (p *ConiksPolicies) Iterate() map[string][]byte {
+	s := reflect.ValueOf(p).Elem()
+	typeOfT := s.Type()
+	fields := make(map[string][]byte, s.NumField())
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		if !f.CanInterface() {
+			continue
+		}
+		switch f.Interface().(type) {
+		case string:
+			fields[typeOfT.Field(i).Name] = []byte(f.Interface().(string))
+		case TimeStamp:
+			fields[typeOfT.Field(i).Name] = util.ULongToBytes(uint64(f.Interface().(TimeStamp)))
+		}
+	}
+	fields["VRFPublic"] = vrf.Public(p.vrfPrivateKey)
+	return fields
 }
 
 // Serialize encodes the policy to a byte array with the following format:
