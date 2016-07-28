a46138199c936395c25b349fe34c0e4bbd8ea391
Vu Quoc Huy <huyvq.c633@gmail.com>
2016-07-29 01:24:12 +0700

    Rename DefaultPolicies to ConiksPolicies and update README

--- merkletree/policy.go
+++ merkletree/policy.go
@@ -14,15 +14,15 @@ type Policies interface {
 	vrfPrivate() *[vrf.SecretKeySize]byte
 }
 
-type DefaultPolicies struct {
+type ConiksPolicies struct {
 	vrfPrivateKey *[vrf.SecretKeySize]byte
 	epochDeadline TimeStamp
 }
 
-var _ Policies = (*DefaultPolicies)(nil)
+var _ Policies = (*ConiksPolicies)(nil)
 
 func NewPolicies(epDeadline TimeStamp, vrfPrivKey *[vrf.SecretKeySize]byte) Policies {
-	return &DefaultPolicies{
+	return &ConiksPolicies{
 		epochDeadline: epDeadline,
 		vrfPrivateKey: vrfPrivKey,
 	}
@@ -30,7 +30,7 @@ func NewPolicies(epDeadline TimeStamp, vrfPrivKey *[vrf.SecretKeySize]byte) Poli
 
 // Serialize encodes the policy to a byte array with the following format:
 // [lib version, cryptographic algorithm in use, epoch deadline]
-func (p *DefaultPolicies) Serialize() []byte {
+func (p *ConiksPolicies) Serialize() []byte {
 	var bs []byte
 	bs = append(bs, []byte(Version)...)                            // lib Version
 	bs = append(bs, []byte(crypto.HashID)...)                      // cryptographic algorithms in use
@@ -39,10 +39,10 @@ func (p *DefaultPolicies) Serialize() []byte {
 	return bs
 }
 
-func (p *DefaultPolicies) vrfPrivate() *[vrf.SecretKeySize]byte {
+func (p *ConiksPolicies) vrfPrivate() *[vrf.SecretKeySize]byte {
 	return p.vrfPrivateKey
 }
 
-func (p *DefaultPolicies) EpochDeadline() TimeStamp {
+func (p *ConiksPolicies) EpochDeadline() TimeStamp {
 	return p.epochDeadline
 }
