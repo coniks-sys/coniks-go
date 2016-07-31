57effe9e94468a132dade343b23fde41363d3d32
Liamsi <Liamsi@users.noreply.github.com>
2016-07-31 22:23:53 +0200

    [WIP]: (stress) tests: - test coverage in utils package

--- utils/util_test.go
+++ utils/util_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"encoding/binary"
 	"math/rand"
 	"testing"
 	"time"
@@ -26,4 +27,36 @@ func TestBitsBytesConvert(t *testing.T) {
 			t.Error("Wrong conversion")
 		}
 	}
+}
+
+func TestIntToBytes(t *testing.T) {
+	numInt := 42
+	b := IntToBytes(numInt)
+	if int(binary.LittleEndian.Uint32(b)) != numInt {
+		t.Fatal("Conversion to bytes looks wrong!")
+	}
+	// TODO It is possible to call `IntToBytes` with a signed int < 0
+	// isn't that problematic?
+	// for instance this will fail:
+	// b := IntToBytes(-42)
+	// if int(binary.LittleEndian.Uint32(b)) != numInt {
+	// 	t.Fatal("Conversion to bytes looks wrong!")
+	// }
+}
+
+func TestULongToBytes(t *testing.T) {
+	numInt := uint64(42)
+	b := ULongToBytes(numInt)
+	if binary.LittleEndian.Uint64(b) != numInt {
+		t.Fatal("Conversion to bytes looks wrong!")
+	}
+}
+
+func TestLongToBytes(t *testing.T) {
+	numInt := int64(42)
+	b := LongToBytes(numInt)
+	if int64(binary.LittleEndian.Uint64(b)) != numInt {
+		t.Fatal("Conversion to bytes looks wrong!")
+	}
+	// TODO similar problem as in TestIntToBytes
 }
