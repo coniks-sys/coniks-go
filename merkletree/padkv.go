cc8b74db472f53a621ac89c05d41ba4121b891a1
Vu Quoc Huy <huyvq.c633@gmail.com>
2016-08-08 17:25:23 +0700

    [WIP] Fix tests

--- merkletree/padkv.go
+++ merkletree/padkv.go
@@ -2,10 +2,15 @@ package merkletree
 
 import (
 	"encoding/binary"
+	"errors"
 
 	"github.com/coniks-sys/coniks-go/crypto/sign"
 	"github.com/coniks-sys/coniks-go/storage/kv"
 	"github.com/coniks-sys/coniks-go/utils"
+)
+
+var (
+	ErrorBadEpochLength = errors.New("[merkletree] Bad epoch length")
 )
 
 // NewPADFromKV creates new PAD with a latest tree stored in the KV db
@@ -33,14 +38,22 @@ func NewPADFromKV(db kv.DB, policies Policies, signKey sign.PrivateKey, length i
 		return nil, err
 	}
 
+	// get policies from db
+	err = policies.LoadFromKV(db, ep)
+	if err != nil {
+		return nil, err
+	}
+
 	// get str from db
 	str := new(SignedTreeRoot)
-	err = str.LoadFromKV(db, policies, signKey, ep)
+	str.tree = pad.tree.Clone()
+	str.Policies = policies
+	err = str.LoadFromKV(db, signKey, ep)
 	if err != nil {
 		return nil, err
 	}
 	pad.latestSTR = str
-	pad.policies = str.Policies
+	pad.policies = policies
 	pad.snapshots[ep] = str
 	pad.loadedEpochs = append(pad.loadedEpochs, ep)
 
