88c2ca1c9c2bd0d416edfef9d9092d517db8c77b
Ismail Khoffi <Liamsi@users.noreply.github.com>
2016-09-09 18:34:13 +0200

    Use a script which runs cover tests on all packages (#85)
    Use a script which runs cover tests on all packages (recursively)
    
    resolves #83

---
+++ test_coverage.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Simplified version of: https://github.com/h12w/gosweep/blob/master/gosweep.sh
+# by Wáng Hǎiliàng
+DIR_SOURCE="$(find . -maxdepth 10 -type f -not -path '*/vendor*' -name '*.go' | xargs -I {} dirname {} | sort | uniq)"
+
+
+# If you want to test in combination with the -race flag on you have to use the atomic mode:
+# echo "mode: atomic" > profile.cov
+echo "mode: count" > profile.cov
+
+# Run test coverage on each subdirectories and merge the coverage profile
+for dir in ${DIR_SOURCE};
+do
+    # change to -covermode=atomic if you want to run in combination with -race:
+    go test -covermode=count -coverprofile=$dir/profile.tmp $dir
+    if [ -f $dir/profile.tmp ]
+    then
+        cat $dir/profile.tmp | tail -n +2 >> profile.cov
+        rm $dir/profile.tmp
+    fi
+done
