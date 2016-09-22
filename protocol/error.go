9af52ac46e5b233c249552c8b62272a8b48cc812
Vu Quoc Huy <huyvq.c633@gmail.com>
2016-09-22 13:58:50 -0700

    New errors used in client side

--- protocol/error.go
+++ protocol/error.go
@@ -15,6 +15,25 @@ const (
 	ErrorMalformedClientMessage
 )
 
+const (
+	Passed ErrorCode = iota + 20
+	ErrorMalformedDirectoryMessage
+	ErrorBadSignature
+	ErrorBadVRFProof
+	ErrorBadIndex
+	ErrorBadMapping
+	ErrorBadSTR
+	ErrorBadCommitment
+	ErrorBadBinding
+)
+
+// ErrorResponses contains error codes that
+// a response can omit the DirectoryResponse.
+var ErrorResponses = map[ErrorCode]bool{
+	ErrorMalformedClientMessage: true,
+	ErrorDirectory:              true,
+}
+
 var (
 	errorMessages = map[ErrorCode]error{
 		Success:                     nil,
@@ -22,6 +41,16 @@ var (
 		ErrorNameExisted:            errors.New("[coniks] Registering identity is already registered"),
 		ErrorNameNotFound:           errors.New("[coniks] Name not found"),
 		ErrorDirectory:              errors.New("[coniks] Directory error"),
+
+		Passed: nil,
+		ErrorMalformedDirectoryMessage: errors.New("[coniks] Malformed directory message"),
+		ErrorBadSignature:              errors.New("[coniks] Directory's signature is invalid"),
+		ErrorBadVRFProof:               errors.New("[coniks] Bad VRF proof"),
+		ErrorBadIndex:                  errors.New("[coniks] Directory returned a bad index"),
+		ErrorBadMapping:                errors.New("[coniks] Returned name-to-key mapping is inconsistent with the root hash"),
+		ErrorBadSTR:                    errors.New("[coniks] The hash chain is inconsistent"),
+		ErrorBadCommitment:             errors.New("[coniks] Bad commitment"),
+		ErrorBadBinding:                errors.New("[coniks] Bad name-to-key binding"),
 	}
 )
 
