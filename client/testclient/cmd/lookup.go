a2126692512180931afa6de3941132e881fc7426
Liamsi <Liamsi@users.noreply.github.com>
2016-09-24 12:42:12 +0200

    [WIP]: rename coniksclient to testclient to better reflect its purpose

--- client/testclient/cmd/lookup.go
+++ client/testclient/cmd/lookup.go
@@ -1,100 +1,20 @@
 package cmd
 
 import (
-	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	"os"
-
-	"github.com/coniks-sys/coniks-go/client"
-	"github.com/coniks-sys/coniks-go/keyserver/testutil"
-	p "github.com/coniks-sys/coniks-go/protocol"
 	"github.com/spf13/cobra"
 )
 
 var lookupCmd = &cobra.Command{
-	Use:   "lookup",
+	Use:   "lockup",
 	Short: "Lookup a name.",
 	Long:  `Lookup the key of some known contact.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		conf := loadConfigOrExit(cmd)
-		name := cmd.Flag("name").Value.String()
-		if len(name) == 0 {
-			cmd.Usage()
-			return
-		}
-		req, err := createLookupRequest(name)
-		if err != nil {
-			fmt.Println("Couldn'r create request!")
-			os.Exit(-1)
-		}
-		resp, err := testutil.NewTCPClient(req)
-		if err != nil {
-			fmt.Println("Error while retrieving repsonse: " + err.Error())
-			os.Exit(-1)
-		}
-		response, errCode := client.UnmarshalResponse(p.KeyLookupType,
-			resp)
-		res, ok := response.(*p.DirectoryProof)
-		if !ok {
-			fmt.Println("Got unexpected response from server!")
-			os.Exit(-1)
-		}
-		switch errCode {
-		case p.Success:
-			c := res.Verify(name, []byte(res.AP.Leaf.Value), 0, nil,
-				conf.SigningPubKey)
-			// verify auth. path:
-			if c != p.Passed || res.AP == nil ||
-				!res.AP.Verify(res.STR.TreeHash) {
-				fmt.Println("Response message didn't pass verification (invalid auth. path).")
-			}
-			if res.TB != nil {
-				tbb := res.TB.Serialize(res.STR.Signature)
-				if !conf.SigningPubKey.Verify(tbb, res.TB.Signature) {
-					fmt.Println("Got invalid TB!")
-				}
-				fmt.Println("Sucess! Got temporary binding (check again after next epoch). Key: [" +
-					string(res.TB.Value) +
-					"] will be inserted at index:\n" +
-					hex.Dump(res.TB.Index))
-			} else {
-				fmt.Println("Sucess! Key bound to name is: [" +
-					string(res.AP.Leaf.Value) + "]")
-				fmt.Println("Index:\n" + hex.Dump(res.AP.Leaf.Index))
-			}
-
-		case p.ErrorMalformedClientMessage:
-			fmt.Println("Server reported malformed client message.")
-		case p.ErrorNameNotFound:
-			c := res.Verify(name, []byte(res.AP.Leaf.Value), 0, nil,
-				conf.SigningPubKey)
-			// verify auth. path:
-			if c != p.Passed || !res.AP.Verify(res.STR.TreeHash) {
-				fmt.Println("Response message didn't pass verification (invalid auth. path).")
-			}
-			fmt.Println("Name isn't registered.")
-		default:
-			fmt.Println(errCode)
-			os.Exit(-1)
-		}
-
+		fmt.Println("TODO: not implmented yet.")
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(lookupCmd)
-	lookupCmd.Flags().StringP("name", "n", "",
-		"User-name of the contact you want to do the look-up for.")
-	lookupCmd.Flags().StringP("config", "c", "config.toml",
-		"Config file for the client (contains the server's initial public key etc.)")
-}
-
-func createLookupRequest(name string) ([]byte, error) {
-	return json.Marshal(&p.Request{
-		Type: p.KeyLookupType,
-		Request: &p.KeyLookupRequest{
-			Username: name,
-		},
-	})
+	lookupCmd.Flags().StringP("name", "n", "", "User-name of the contact you want to do the look-up for.")
 }
--- client/testclient/cmd/register.go
+++ client/testclient/cmd/register.go
@@ -2,14 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"net"
-	"os"
-
-	"encoding/json"
-
-	"github.com/coniks-sys/coniks-go/client"
-	"github.com/coniks-sys/coniks-go/keyserver/testutil"
-	p "github.com/coniks-sys/coniks-go/protocol"
 	"github.com/spf13/cobra"
 )
 
@@ -18,99 +10,26 @@ var registerCmd = &cobra.Command{
 	Short: "Register a name-to-key binding.",
 	Long: `Register a new name-to-key binding on the CONIKS-server.
 Note that, for the registration to work, the test-client needs to be running on the same machine as the key-server.
-A real client would first register an app here: https://apps.twitter.com (the UI should make this possible without actually leaving the client application). Then, the client would use the oauth1 API (find the tokens on the [Keys and Access Tokens] tab of the aforementioned page) and contact the (twitter-)bot for a registration request. The twitter bot is also accessible from outside.
+A real client would first register an app here: https://apps.twitter.com (the UI should make this possible without actually leaving the client application). Then, the client would use the oauth1 API (find the tokens on the [Keys and Access Tokens] tab of the aforementioned page) and contact the (twitter-)bot for a registration request. The twitter bit is also accessible from outside.
 For more information consult: https://godoc.org/github.com/coniks-sys/coniks-go/bots
 
 Example call:
-  coniksclient register --name Alice@twitter --key fake_test_key`,
+  coniksclient register -name Alice@twitter -key fake_test_key`,
 	Run: func(cmd *cobra.Command, args []string) {
-		conf := loadConfigOrExit(cmd)
-		name := cmd.Flag("name").Value.String()
-		key := cmd.Flag("key").Value.String()
-		if len(name) == 0 || len(key) == 0 {
+		if len(cmd.Flag("name").Value.String()) == 0 ||
+			len(cmd.Flag("key").Value.String()) == 0 {
 			cmd.Usage()
 			return
 		}
-		msg, err := createRegistrationMsg(name, key)
-		if err != nil {
-			fmt.Println("Couldn't marshal registration request!")
-			os.Exit(-1)
-		}
-
-		addr, err := net.ResolveUnixAddr("unix", cmd.Flag("socket").Value.String())
-		if err != nil {
-			fmt.Println("Invalid socket address: " + err.Error())
-			os.Exit(1)
-		}
-		res, err := testutil.NewUnixClient(msg, addr)
-		if err != nil {
-			fmt.Println("Error while receiving response: " + err.Error())
-		}
-
-		response, errCode := client.UnmarshalResponse(p.RegistrationType,
-			res)
-		switch errCode {
-		case p.Success:
-			res, ok := response.(*p.DirectoryProof)
-			if !ok {
-				fmt.Println("Got unexpected response from server!")
-				os.Exit(-1)
-			}
-			// TODO why are the implementations for this verification
-			// method all empty? (see: protocol/message.go)
-			c := res.Verify(name, []byte(key), 0, nil,
-				conf.SigningPubKey)
-			// verify auth. path:
-			if c != p.Passed ||
-				res.AP == nil ||
-				!res.AP.Verify(res.STR.TreeHash) {
-				fmt.Println("Response message didn't pass verification (invalid auth. path).")
-			}
-			// verify signature on TB:
-			tbb := res.TB.Serialize(res.STR.Signature)
-			if !conf.SigningPubKey.Verify(tbb, res.TB.Signature) {
-				fmt.Println("Couldn't verify signature of temporary binding.")
-			}
-			fmt.Println("Succesfully registered name: " + name)
-			// TODO Where should the client save seen STRs and the TB?
-			// (or should it wait till the next epoch to see if the
-			// TB was actually inserted?)
-		case p.ErrorNameExisted:
-			// Key-change isn't currently supported; see:
-			// https://github.com/coniks-sys/coniks-go/issues/92
-			fmt.Println("Name is already registered.")
-		case p.ErrorDirectory:
-			// From a usability perspective: how would a real
-			// client deal with such an error? Retry?
-			fmt.Println("Internal server error.")
-		case p.ErrorMalformedClientMessage:
-			fmt.Println("Server reported an invalid client request!")
-		}
+		fmt.Println(cmd.Flag("socket").Value.String())
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(registerCmd)
-	registerCmd.Flags().StringP("name", "n", "",
-		"Username you want to register with the CONIKS server.")
-
+	registerCmd.Flags().StringP("name", "n", "", "Username you want to register with the CONIKS server.")
 	// TODO if this test-client should be able to handle real key-material,
-	// the client should generate a new key instead. For testing purposes
-	// strings are more convenient, though.
-	registerCmd.Flags().StringP("key", "k", "",
-		"Key-material you want to bind to the user name.")
-	registerCmd.Flags().StringP("socket", "s", "/tmp/coniks.sock",
-		"The socket on which the client can directly connect to the key-server.")
-	registerCmd.Flags().StringP("config", "c", "config.toml",
-		"Config file for the client (contains the server's initial public key etc.)")
-}
-
-func createRegistrationMsg(name, key string) ([]byte, error) {
-	return json.Marshal(&p.Request{
-		Type: p.RegistrationType,
-		Request: &p.RegistrationRequest{
-			Username: name,
-			Key:      []byte(key), // TODO maybe generate a new key here
-		},
-	})
+	// it should be possible to pass a file containing the key instead.
+	registerCmd.Flags().StringP("key", "k", "", "Key-material you want to bind to the user name.")
+	registerCmd.Flags().StringP("socket", "s", "/tmp/coniks.sock", "The socket on which the client connects to ")
 }
