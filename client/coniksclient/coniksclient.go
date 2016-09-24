3e871737bff6266bba6fcabc524871d8bebfd342
Liamsi <Liamsi@users.noreply.github.com>
2016-09-24 12:42:12 +0200

    [WIP]: rename coniksclient to testclient to better reflect its purpose

--- client/coniksclient/coniksclient.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/coniks-sys/coniks-go/client/coniksclient/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
---
+++ client/testclient/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/coniks-sys/coniks-go/client/testclient/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
