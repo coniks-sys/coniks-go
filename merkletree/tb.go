8be77b29132246f36af5b4fab1cabee255672ffd
Vu Quoc Huy <huyvq.c633@gmail.com>
2016-10-12 18:38:20 +0700

    TemporaryBinding should include the issued epoch for verifications

--- merkletree/tb.go
+++ merkletree/tb.go
@@ -1,17 +1,26 @@
 package merkletree
 
-import "github.com/coniks-sys/coniks-go/crypto/sign"
+import (
+	"bytes"
+
+	"github.com/coniks-sys/coniks-go/crypto/sign"
+)
 
 type TemporaryBinding struct {
-	Index     []byte
-	Value     []byte
-	Signature []byte
+	// IssuedEpoch would not be included in the serialization.
+	// To verify the validation of IssuedEpoch, we can compare it
+	// against the epoch of STR included in the serialization.
+	IssuedEpoch uint64
+	Index       []byte
+	Value       []byte
+	Signature   []byte
 }
 
-func NewTB(key sign.PrivateKey, strSig, index, value []byte) *TemporaryBinding {
+func NewTB(key sign.PrivateKey, ep uint64, strSig, index, value []byte) *TemporaryBinding {
 	tb := &TemporaryBinding{
-		Index: index,
-		Value: value,
+		IssuedEpoch: ep,
+		Index:       index,
+		Value:       value,
 	}
 	tbPreSig := tb.Serialize(strSig)
 	tb.Signature = key.Sign(tbPreSig)
@@ -24,4 +33,13 @@ func (tb *TemporaryBinding) Serialize(strSig []byte) []byte {
 	tbBytes = append(tbBytes, tb.Index...)
 	tbBytes = append(tbBytes, tb.Value...)
 	return tbBytes
+}
+
+func (tb *TemporaryBinding) Verify(ap *AuthenticationPath) bool {
+	// compare TB's index with authentication path's index (after Update)
+	if !bytes.Equal(ap.LookupIndex, tb.Index) ||
+		!bytes.Equal(ap.Leaf.Value, tb.Value) {
+		return false
+	}
+	return true
 }
