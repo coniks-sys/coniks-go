46a16c4d4dfc8753bd4395927dcc16b672148222
Marcela Melara <msmelara@gmail.com>
2016-10-23 19:14:12 -0400

    Ensure start epoch of KeyLookupInEpoch and Monitoring requests is in PAD - Otherwise directory.LookupInEpoch() fails with ErrorDirectory, though a start epoch that is > the latest epoch indicates a malformed client message. - Closes #108

--- protocol/directory_test.go
+++ protocol/directory_test.go
@@ -285,15 +285,53 @@ func TestDirectoryKeyLookupInEpoch(t *testing.T) {
 	}
 }
 
-func TestHandleOps(t *testing.T) {
+func TestDirectoryKeyLookupInEpochBadEpoch(t *testing.T) {
+	N := 3
+
 	d, _ := NewTestDirectory(t, false)
-	// Send an invalid KeyLookupInEpochRequest
+	for i := 0; i < N; i++ {
+		d.Update()
+	}
+
+	// Send an invalid KeyLookupInEpochRequest (epoch > d.LatestEpoch())
+	// Expect ErrorMalformedClientMessage
+	req := &Request{
+		Type:    KeyLookupInEpochType,
+		Request: &KeyLookupInEpochRequest{"alice", uint64(6)},
+	}
+
+	_, err := d.HandleOps(req)
+	if err != ErrorMalformedClientMessage {
+		t.Fatal("Expect error", ErrorMalformedClientMessage, "got", err)
+	}
+}
+
+func TestMonitoringBadStartEpoch(t *testing.T) {
+	N := 3
+
+	d, _ := NewTestDirectory(t, false)
+	for i := 0; i < N; i++ {
+		d.Update()
+	}
+
+	// Send an invalid MonitoringRequest (startEpoch > d.LatestEpoch())
 	// Expect ErrorMalformedClientMessage
 	req := &Request{
 		Type:    MonitoringType,
-		Request: &MonitoringRequest{"alice", uint64(2), uint64(0)},
+		Request: &MonitoringRequest{"alice", uint64(6), uint64(10)},
 	}
 	_, err := d.HandleOps(req)
+	if err != ErrorMalformedClientMessage {
+		t.Fatal("Expect error", ErrorMalformedClientMessage, "got", err)
+	}
+
+	// Send an invalid MonitoringRequest (startEpoch > EndEpoch)
+	// Expect ErrorMalformedClientMessage
+	req = &Request{
+		Type:    MonitoringType,
+		Request: &MonitoringRequest{"alice", uint64(2), uint64(0)},
+	}
+	_, err = d.HandleOps(req)
 	if err != ErrorMalformedClientMessage {
 		t.Fatal("Expect error", ErrorMalformedClientMessage, "got", err)
 	}
