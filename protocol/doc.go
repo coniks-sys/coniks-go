c59a70cf85d23f089911a896c8b9c060ae2c19aa
Marcela Melara <msmelara@gmail.com>
2016-10-27 16:35:17 -0400

    Add message, consistency check, error modules to doc.go

--- protocol/doc.go
+++ protocol/doc.go
@@ -8,6 +8,13 @@ an API for maintaining an auditable, privacy-preserving key directory on a
 server, as well as an API for checking the consistency of the directory
 at the client.
 
+Consistency Checks
+
+This module implements all consistency checks perfomed by a CONIKS client
+on directory proofs received from a CONIKS server. These operations
+include the verification of username-to-key bindings (authentication paths),
+and non-equivocation checks (signed tree root).
+
 Directory
 
 This module implements a CONIKS key directory that a CONIKS key server
@@ -15,5 +22,19 @@ maintains. A directory is a publicly auditable, tamper-evident,
 privacy-preserving data structure that contains mappings from usernames
 to public keys. It currently supports registration of new mappings,
 current key lookups, historical key lookups, and monitoring of mappings.
+
+Error
+
+This module defines the constants representing the types
+of errors that a CONIKS server may return to a client,
+and the results of a consistency check or cryptographic verification
+that a CONIKS client performs.
+
+Message
+
+This module defines the message format of the CONIKS client requests
+and corresponding CONIKS server responses for each CONIKS protocol.
+It also provides constructors for the response messages for each
+protocol.
 */
 package protocol
