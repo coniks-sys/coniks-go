d8342b6f8d27b87310f8394e166559c4fcdf3aab
Vu Quoc Huy <huyvq.c633@gmail.com>
2016-11-05 14:50:29 +0700

    Refactor tests

--- protocol/consistencychecks_test.go
+++ protocol/consistencychecks_test.go
@@ -3,9 +3,33 @@ package protocol
 import "testing"
 
 var (
-	uname = "alice"
-	key   = []byte("key")
+	key = []byte("key")
 )
+
+func doRequestAndVerify(d *ConiksDirectory,
+	cc *ConsistencyChecks,
+	requestType int, name string) error {
+
+	switch requestType {
+	case RegistrationType:
+		request := &RegistrationRequest{
+			Username: name,
+			Key:      key,
+		}
+		res, _ := d.Register(request)
+		return cc.UpdateConsistency(requestType, res, name, key)
+	case KeyLookupType:
+		request := &KeyLookupRequest{
+			Username: name,
+		}
+		res, _ := d.KeyLookup(request)
+		return cc.UpdateConsistency(requestType, res, name, key)
+	case MonitoringType:
+	case KeyLookupInEpochType:
+	}
+
+	panic("Unknown request type")
+}
 
 func TestVerifyWithError(t *testing.T) {
 	d, pk := NewTestDirectory(t, true)
@@ -17,10 +41,7 @@ func TestVerifyWithError(t *testing.T) {
 
 	cc := NewCC(&str, true, pk)
 
-	res, _ := d.Register(&RegistrationRequest{
-		Username: uname,
-		Key:      key})
-	if err := cc.UpdateConsistency(RegistrationType, res, uname, key); err != ErrorBadSTR {
+	if err := doRequestAndVerify(d, cc, RegistrationType, "alice"); err != ErrorBadSTR {
 		t.Fatal("Expect", ErrorBadSTR, "got", err)
 	}
 }
@@ -30,10 +51,7 @@ func TestVerifyRegistrationResponseWithTB(t *testing.T) {
 
 	cc := NewCC(d.LatestSTR(), true, pk)
 
-	res, _ := d.Register(&RegistrationRequest{
-		Username: uname,
-		Key:      key})
-	if cc.UpdateConsistency(RegistrationType, res, uname, key) != PassedWithAProofOfAbsence {
+	if doRequestAndVerify(d, cc, RegistrationType, "alice") != PassedWithAProofOfAbsence {
 		t.Fatal("Unexpected verification result")
 	}
 
@@ -42,27 +60,20 @@ func TestVerifyRegistrationResponseWithTB(t *testing.T) {
 	}
 
 	// test error name existed
-	res, err := d.Register(&RegistrationRequest{
-		Username: uname,
-		Key:      key})
-	if err != ErrorNameExisted {
-		t.Fatal("Expect error code", ErrorNameExisted, "got", err)
-	}
-	// expect a proof of absence since this binding wasn't included in this epoch
-	if err := cc.UpdateConsistency(RegistrationType, res, uname, key); err != PassedWithAProofOfAbsence {
+	if doRequestAndVerify(d, cc, RegistrationType, "alice") != PassedWithAProofOfAbsence {
 		t.Fatal("Unexpected verification result")
 	}
 
 	// test error name existed with different key
-	res, err = d.Register(&RegistrationRequest{
-		Username: uname,
+	res, err := d.Register(&RegistrationRequest{
+		Username: "alice",
 		Key:      []byte{1, 2, 3},
 	})
 	if err != ErrorNameExisted {
 		t.Fatal("Expect error code", ErrorNameExisted, "got", err)
 	}
 	// expect a proof of absence since this binding wasn't included in this epoch
-	if err := cc.UpdateConsistency(RegistrationType, res, uname, key); err != PassedWithAProofOfAbsence {
+	if err := cc.UpdateConsistency(RegistrationType, res, "alice", key); err != PassedWithAProofOfAbsence {
 		t.Fatal("Unexpected verification result")
 	}
 
@@ -74,13 +85,7 @@ func TestVerifyRegistrationResponseWithTB(t *testing.T) {
 	// Since the fulfilled promise verification would be perform
 	// when the client is monitoring, we do _not_ expect a TB's verification here.
 	d.Update()
-	res, err = d.Register(&RegistrationRequest{
-		Username: uname,
-		Key:      key})
-	if err != ErrorNameExisted {
-		t.Fatal("Expect error code", ErrorNameExisted, "got", err)
-	}
-	if cc.UpdateConsistency(RegistrationType, res, uname, key) != PassedWithAProofOfInclusion {
+	if doRequestAndVerify(d, cc, RegistrationType, "alice") != PassedWithAProofOfInclusion {
 		t.Fatal("Unexpected verification result")
 	}
 }
@@ -90,19 +95,10 @@ func TestVerifyFullfilledPromise(t *testing.T) {
 
 	cc := NewCC(d.LatestSTR(), true, pk)
 
-	res, _ := d.Register(&RegistrationRequest{
-		Username: "alice",
-		Key:      key})
-
-	if cc.UpdateConsistency(RegistrationType, res, "alice", key) != PassedWithAProofOfAbsence {
+	if doRequestAndVerify(d, cc, RegistrationType, "alice") != PassedWithAProofOfAbsence {
 		t.Fatal("Unexpected verification result")
 	}
-
-	res, _ = d.Register(&RegistrationRequest{
-		Username: "bob",
-		Key:      key})
-
-	if cc.UpdateConsistency(RegistrationType, res, "bob", key) != PassedWithAProofOfAbsence {
+	if doRequestAndVerify(d, cc, RegistrationType, "bob") != PassedWithAProofOfAbsence {
 		t.Fatal("Unexpected verification result")
 	}
 
@@ -112,55 +108,16 @@ func TestVerifyFullfilledPromise(t *testing.T) {
 
 	d.Update()
 
-	res, err := d.KeyLookup(&KeyLookupRequest{
-		Username: "alice"})
-
-	if err != Success {
-		t.Fatal("Expect error code", Success, "got", err)
-	}
-	if err := cc.UpdateConsistency(KeyLookupType, res, "alice", key); err != PassedWithAProofOfInclusion {
-		t.Fatal("Unexpected verification result", "got", err)
+	for i := 0; i < 2; i++ {
+		if doRequestAndVerify(d, cc, KeyLookupType, "alice") != PassedWithAProofOfInclusion {
+			t.Fatal("Unexpected verification result")
+		}
 	}
 
 	// should contain the TBs of "bob"
 	if len(cc.TBs) != 1 || cc.TBs["bob"] == nil {
 		t.Error("Expect the directory to insert the binding as promised")
 	}
-
-	// register new binding and forget to verify the fulfilled promise
-	res, err = d.Register(&RegistrationRequest{
-		Username: "eve",
-		Key:      key})
-
-	if cc.UpdateConsistency(RegistrationType, res, "eve", key) != PassedWithAProofOfAbsence {
-		t.Fatal("Unexpected verification result")
-	}
-	if len(cc.TBs) != 2 || cc.TBs["eve"] == nil {
-		t.Fatal("Expect the directory to return signed promises")
-	}
-
-	d.Update()
-
-	// bypass the hash chain verification
-	cc.SavedSTR = d.LatestSTR()
-
-	d.Update()
-
-	res, err = d.KeyLookup(&KeyLookupRequest{
-		Username: "eve"})
-
-	if err != Success {
-		t.Fatal("Expect error code", Success, "got", err)
-	}
-	if err := cc.UpdateConsistency(KeyLookupType, res, "eve", key); err != PassedWithAProofOfInclusion {
-		t.Fatal("Unexpected verification result", "got", err)
-	}
-
-	// TODO: expect eve's TB was instered as promise
-	// should be resolve when we address monitoring
-	// if len(cc.TBs) != 0 {
-	// 	t.Error("Expect the directory to insert the binding as promised")
-	// }
 }
 
 func TestVerifyKeyLookupResponseWithTB(t *testing.T) {
@@ -169,43 +126,27 @@ func TestVerifyKeyLookupResponseWithTB(t *testing.T) {
 	cc := NewCC(d.LatestSTR(), true, pk)
 
 	// do lookup first
-	res, err := d.KeyLookup(&KeyLookupRequest{uname})
-	if err != ErrorNameNotFound {
-		t.Fatal("Expect error code", ErrorNameNotFound, "got", err)
-	}
-	if cc.UpdateConsistency(KeyLookupType, res, uname, key) != PassedWithAProofOfAbsence {
+	if doRequestAndVerify(d, cc, KeyLookupType, "alice") != PassedWithAProofOfAbsence {
 		t.Fatal("Unexpected verification result")
 	}
 
 	// register
-	res, _ = d.Register(&RegistrationRequest{
-		Username: uname,
-		Key:      key})
-	// do lookup in the same epoch
-	res, err = d.KeyLookup(&KeyLookupRequest{uname})
-	if err != Success {
-		t.Fatal("Expect error code", Success, "got", err)
+	if doRequestAndVerify(d, cc, RegistrationType, "alice") != PassedWithAProofOfAbsence {
+		t.Fatal("Unexpected verification result")
 	}
-	if cc.UpdateConsistency(KeyLookupType, res, uname, key) != PassedWithAProofOfAbsence {
+	// do lookup in the same epoch
+	if doRequestAndVerify(d, cc, KeyLookupType, "alice") != PassedWithAProofOfAbsence {
 		t.Fatal("Unexpected verification result")
 	}
 
 	// do lookup in the different epoch
 	d.Update()
-	res, err = d.KeyLookup(&KeyLookupRequest{uname})
-	if err != Success {
-		t.Fatal("Expect error code", Success, "got", err)
-	}
-	if cc.UpdateConsistency(KeyLookupType, res, uname, key) != PassedWithAProofOfInclusion {
+	if doRequestAndVerify(d, cc, KeyLookupType, "alice") != PassedWithAProofOfInclusion {
 		t.Fatal("Unexpected verification result")
 	}
 
 	// test error name not found
-	res, err = d.KeyLookup(&KeyLookupRequest{"bob"})
-	if err != ErrorNameNotFound {
-		t.Fatal("Expect error code", Success, "got", err)
-	}
-	if cc.UpdateConsistency(KeyLookupType, res, "bob", nil) != PassedWithAProofOfAbsence {
+	if doRequestAndVerify(d, cc, KeyLookupType, "bob") != PassedWithAProofOfAbsence {
 		t.Fatal("Unexpected verification result")
 	}
 }
