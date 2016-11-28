92ec0ae33e7bf0324de72c71c30c23dd1750c89c
Vu Quoc Huy <huyvq.c633@gmail.com>
2016-11-28 23:05:59 +0700

    Remove summary of bot/coniksbot

--- bots/coniksbot/coniksbot.go
+++ bots/coniksbot/coniksbot.go
@@ -1,5 +1,3 @@
-// Package coniksbot provides the main executable for
-// a CONIKS account verification proxy for Twitter accounts.
 package main
 
 import "github.com/coniks-sys/coniks-go/bots/coniksbot/internal/cmd"
