a0c2181423ccec8a397d59dae7a1e7abfc9e13cc
Arlo Breault <arlolra@gmail.com>
2016-12-19 11:48:27 -0800

    Add comments to protocol/str.go

--- protocol/str.go
+++ protocol/str.go
@@ -2,11 +2,14 @@ package protocol
 
 import "github.com/coniks-sys/coniks-go/merkletree"
 
+// DirSTR disambiguates merkletree.SignedTreeRoot's AssocData interface,
+// for the purpose of exporting and unmarshalling.
 type DirSTR struct {
 	merkletree.SignedTreeRoot
 	Policies *Policies
 }
 
+// NewDirSTR constructs a new DirSTR from a merkletree.SignedTreeRoot
 func NewDirSTR(str *merkletree.SignedTreeRoot) *DirSTR {
 	return &DirSTR{
 		*str,
@@ -14,6 +17,7 @@ func NewDirSTR(str *merkletree.SignedTreeRoot) *DirSTR {
 	}
 }
 
+// VerifyHashChain wraps merkletree.SignedTreeRoot.VerifyHashChain
 func (str *DirSTR) VerifyHashChain(savedSTR *DirSTR) bool {
 	return str.SignedTreeRoot.VerifyHashChain(&savedSTR.SignedTreeRoot)
 }
