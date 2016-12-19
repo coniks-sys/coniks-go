8d42521df3df4d33939335f7cd03147a5e0f7d5f
Arlo Breault <arlolra@gmail.com>
2016-12-18 22:35:18 -0800

    Use protocol.DirSTR for policy unmarshalling
     * Closes #135

--- merkletree/str.go
+++ merkletree/str.go
@@ -3,8 +3,6 @@ package merkletree
 import (
 	"bytes"
 
-	"encoding/json"
-
 	"github.com/coniks-sys/coniks-go/crypto"
 	"github.com/coniks-sys/coniks-go/crypto/sign"
 	"github.com/coniks-sys/coniks-go/utils"
@@ -29,44 +27,7 @@ type SignedTreeRoot struct {
 	PreviousEpoch   uint64
 	PreviousSTRHash []byte
 	Signature       []byte
-	Ad              AssocData
-}
-
-// TODO(arlolra): Look into using a DirSTR instead, to avoid this mess.
-
-// RawAd makes json.RawMessage implement the AssocData interface.
-type RawAd json.RawMessage
-
-// Serialize should never be called.
-func (r RawAd) Serialize() []byte {
-	panic("[str] Should never be called")
-	return nil
-}
-
-// UnmarshalJSON returns the STR with a json.RawMessage as AssocData for
-// further unmarshalling by the caller.
-func (str *SignedTreeRoot) UnmarshalJSON(m []byte) error {
-	type Str struct {
-		tree            *MerkleTree
-		TreeHash        []byte
-		Epoch           uint64
-		PreviousEpoch   uint64
-		PreviousSTRHash []byte
-		Signature       []byte
-		Ad              json.RawMessage
-	}
-	hStr := &Str{}
-	if err := json.Unmarshal(m, hStr); err != nil {
-		return err
-	}
-	str.tree = hStr.tree
-	str.TreeHash = hStr.TreeHash
-	str.Epoch = hStr.Epoch
-	str.PreviousEpoch = hStr.PreviousEpoch
-	str.PreviousSTRHash = hStr.PreviousSTRHash
-	str.Signature = hStr.Signature
-	str.Ad = RawAd(hStr.Ad)
-	return nil
+	Ad              AssocData `json:"-"`
 }
 
 // NewSTR constructs a SignedTreeRoot with the given signing key pair,
---
+++ protocol/str.go
@@ -0,0 +1,23 @@
+package protocol
+
+import "github.com/coniks-sys/coniks-go/merkletree"
+
+// DirSTR disambiguates merkletree.SignedTreeRoot's AssocData interface,
+// for the purpose of exporting and unmarshalling.
+type DirSTR struct {
+	*merkletree.SignedTreeRoot
+	Policies *Policies
+}
+
+// NewDirSTR constructs a new DirSTR from a merkletree.SignedTreeRoot
+func NewDirSTR(str *merkletree.SignedTreeRoot) *DirSTR {
+	return &DirSTR{
+		str,
+		str.Ad.(*Policies),
+	}
+}
+
+// VerifyHashChain wraps merkletree.SignedTreeRoot.VerifyHashChain
+func (str *DirSTR) VerifyHashChain(savedSTR *DirSTR) bool {
+	return str.SignedTreeRoot.VerifyHashChain(savedSTR.SignedTreeRoot)
+}
